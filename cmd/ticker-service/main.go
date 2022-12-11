2a3e04255682df03e12facb6c7bfb7668de8a223
Enda Kelly <kelly.enda@gmail.com>
2022-12-11 15:01:34 +0000

    added: influxdb via go

--- cmd/ticker-service/main.go
+++ cmd/ticker-service/main.go
@@ -4,16 +4,37 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
+	"time"
+
+	"github.com/endk17/go-crypto-ticker/cmd/ticker-service/config"
+	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
+	"github.com/k0kubun/pp/v3"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
+
+func init() {
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339})
+}
 
 func main() {
 	// Initial Set up: web server
-
 	pingHandler := func(w http.ResponseWriter, req *http.Request) {
 		io.WriteString(w, "pong!\n")
 	}
-
 	http.HandleFunc("/ping", pingHandler)
 	log.Println("Listing for requests at http://localhost:8000/ping")
 	log.Fatal(http.ListenAndServe(":8000", nil))
+
+	cfg, err := config.Read()
+	if err != nil {
+		log.Fatal().Err(err).Send()
+	}
+	pp.Println(cfg)
+
+	// Create new influxdb client with default option for server url authenticate by token
+	influxdb := influxdb2.NewClient(cfg.InfluxDB.URL, cfg.InfluxDB.Token)
+	defer influxdb.Close()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,18 @@
 module github.com/endk17/go-crypto-ticker
 
 go 1.19
+
+require (
+	github.com/deepmap/oapi-codegen v1.8.2 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/influxdata/influxdb-client-go/v2 v2.12.1 // indirect
+	github.com/influxdata/line-protocol v0.0.0-20200327222509-2487e7298839 // indirect
+	github.com/k0kubun/pp/v3 v3.2.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rs/zerolog v1.28.0 // indirect
+	golang.org/x/net v0.0.0-20210119194325-5f4716e94777 // indirect
+	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
